65f1743cb9cdfbc21995c0605a6022f2edd2a4ad
Ronaldo Pinto <ronaldotpinto@gmail.com>
2020-06-11 18:03:08 -0300

    Aula 33

--- src/main/java/TesteAlert.java
+++ src/main/java/TesteAlert.java
@@ -13,7 +13,7 @@ public class TesteAlert {
 	String url = "file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html";
 	
 	@Test
-	public void buscarTextoPagina(){
+	public void interagirAlertSimples(){
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().setSize(new Dimension(1220, 765));
 		driver.get(url);
@@ -28,6 +28,29 @@ public class TesteAlert {
 		driver.findElement(By.id("elementosForm:nome")).sendKeys(textoAlert);
 		
 		driver.quit();
+	}
+	
+	@Test
+	public void interagirAlertConfirm(){
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1220, 765));
+		driver.get(url);
+		
+		driver.findElement(By.id("confirm")).click();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alert.getText());
+		alert.accept();
+		Assert.assertEquals("Confirmado", alert.getText());
+		alert.accept();
+		
+		driver.findElement(By.id("confirm")).click();
+		alert = driver.switchTo().alert();
+		Assert.assertEquals("Confirm Simples", alert.getText());
+		alert.dismiss();
+		Assert.assertEquals("Negado", alert.getText());
+		alert.accept();
+		
+		driver.quit();		
 	}
 	
 }
