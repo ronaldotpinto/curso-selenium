e6106f11428a855d694e6d481e148219cde4fdea
Ronaldo Pinto <ronaldotpinto@gmail.com>
2020-06-11 18:10:08 -0300

    Aula 34

--- src/main/java/TesteAlert.java
+++ src/main/java/TesteAlert.java
@@ -53,4 +53,23 @@ public class TesteAlert {
 		driver.quit();		
 	}
 	
+	@Test
+	public void interagirAlertPrompt(){
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().setSize(new Dimension(1220, 765));
+		driver.get(url);
+		
+		driver.findElement(By.id("prompt")).click();
+		Alert alert = driver.switchTo().alert();
+		Assert.assertEquals("Digite um numero", alert.getText());
+		alert.sendKeys("12");
+		alert.accept();
+		Assert.assertEquals("Era 12?", alert.getText());
+		alert.accept();
+		Assert.assertEquals(":D", alert.getText());
+		alert.accept();
+		
+		driver.quit();
+	}
+	
 }
